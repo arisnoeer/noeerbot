24bbf96d1d143fc7c04ac6774d0915b5594a36d7
arisnoeer <76475296+arisnoeer@users.noreply.github.com>
2021-01-02 10:07:23 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -531,14 +531,15 @@ client.on('group-participants-update', async (anu) => {
 							})
 							.on('error', function (err) {
 								console.log(`Error : ${err}`)
-								
+								fs.unlinkSync(media)
 								reply(mess.error.stick)
 							})
 							.on('end', function () {
 								console.log('Finish')
-								
-								client.sendMessage(from, sticker, {quoted: mek})
-								
+								buff = fs.readFileSync(ran)
+								client.sendMessage(from, buff, sticker, {quoted: mek})
+								fs.unlinkSync(media)
+								fs.unlinkSync(ran)
 							})
 							.addOutputOptions([`-vcodec`,`libwebp`,`-vf`,`scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
 							.toFormat('webp')
